c9ac4489eda8b92639ff3155065182e90a2ff904
Moose <Davis>
2021-07-21 11:55:36 -0500

    push code

--- app/serializers/airline_serializer.rb
+++ app/serializers/airline_serializer.rb
@@ -1,6 +1,6 @@
 class AirlineSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :name, :image_url, :slug
+	include FastJsonapi::ObjectSerializer
+  	attributes :name, :image_url, :slug
 
-  has_many :reviews
+  	has_many :reviews
 end
