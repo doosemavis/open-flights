0554c2bc613d7908a0dc18b59ace4a8541315c42
Moose <Davis>
2021-07-21 12:59:45 -0500

    pause on coding tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
 
   namespace :api do
 	namespace :v1 do
-      	resources :airlines, params: :slug 
-      	resources :reviews, only: [:create, :destroy]
+      	resources :airlines, param: :slug 
+      	resources :reviews, only: %i[:create, :destroy]
     	end 
   end 
 
