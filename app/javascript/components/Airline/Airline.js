056a0db80f51283662d7977d029e033f1b38450f
Moose <Davis>
2021-07-22 12:44:33 -0500

    omg so much code

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,6 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios';
 import Header from './Header';
+import ReviewForm from './ReviewForm';
+import Review from './Review';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+     margin-left: auto;
+     margin-right: auto;
+     display: grid;
+     grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+     background: #fff;
+     height: 100vh;
+     overflow: scroll;
+
+     &:last-child {
+          background: #000;
+     }
+`
+const Main = styled.div`
+     padding-left: 50px;
+`
+
 
 const Airline = (props) => {
      const [ airline, setAirline ] = useState({})
@@ -14,27 +37,78 @@ const Airline = (props) => {
          axios.get(url)
          .then( resp => {
               setAirline( resp.data ) 
-              setLoaded(true)
+              setLoaded( true )
           })
-         .then( resp => console.log(resp) )
+         .then( resp => console.log( resp ) )
      }, [] )
 
+     const handleChange = (e) => {
+          e.preventDefault()
+
+          setReview(Object.assign( { }, review, { [e.target.name]: e.target.value } ) )
+     }
+
+     const handleSubmit = (e) => {
+          e.preventDefault()
+
+          const csrfToken = document.querySelector( ' [ name=csrf-token] ' ).content
+          axios.defaults.headers.common[ ' X-CSRF-TOKEN' ] = csrfToken 
+
+          const airline_id = airline.data.id 
+          axios.post('api/v1/reviews', { review, airline_id } )
+          .then( resp => {
+               const included = [ ...airline.included, resp.data.data ]
+               setAirline( { ...airline, included } ) 
+               setReview( { title: '', description: '', score: 0 } )
+          } )
+          .catch( resp => { } )
+     }
+
+     const setRating = ( score, e ) => {
+          e.preventDefault()
+
+          setReview( { ...review, score } )
+     }
+
+     let reviews;
+     if ( loaded && airline.included ) {
+          reviews = airline.included.map( (item, index ) => {
+               return (
+                    <Review 
+                         key={index}
+                         attributes={item.attributes}
+                    /> 
+               )
+          })
+     }
+
+
      return (
-          <div className="wrapper"> 
-               <div className="column">
-                    {
-                         loaded &&
-                         <Header 
-                              attributes={ airline.data.attributes }
-                              reviews={ airline.included }
-                         /> 
-                    }
-                    <div className="reviews"></div>
-               </div> 
-               <div className="column">
-                    <div className="review-form"> [ Reivew form goes here. ] </div>
-               </div>
-          </div> 
+          < Wrapper > 
+               {
+                    loaded &&
+                    <Fragment>
+                         < Column >
+                              <Main>
+                                        <Header 
+                                             attributes={ airline.data.attributes }
+                                             reviews={ airline.included }
+                                        /> 
+                                   { reviews }
+                              </Main>
+                         </ Column > 
+                         < Column >
+                              <ReviewForm 
+                                   handleChange={ handleChange }
+                                   handleSubmit={ handleSubmit }
+                                   setRating={ setRating }
+                                   attributes={ airline.data.attributes }
+                                   review={ review }
+                              />
+                         </ Column >
+                     </Fragment>
+               }
+          </Wrapper > 
      )
 }
 
